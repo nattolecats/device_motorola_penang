31bec3b6b888a25263b5351354444e62e638e2b3
lahaina <nattolecats@gmail.com>
2023-07-18 17:43:05 +0900

    cypfr: vendorsetup: Add missing more repos * Kernel source * Prebuilt vendor image

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,8 +8,17 @@ croot
 KERNEL=device/motorola/cypfr-kernel/kernel
 if ! [ -a $KERNEL ]; then git clone https://github.com/nattolecats/device_motorola_cypfr-kernel device/motorola/cypfr-kernel ; fi
 
+KERNEL_SOURCE=kernel/motorola/msm-5.4
+if ! [ -a $KERNEL_SOURCE ]; then git clone https://github.com/nattolecats/kernel_motorola_msm-5.4 kernel/motorola/msm-5.4 ; fi
+
 VENDOR=vendor/motorola/cypfr/cypfr-vendor.mk
 if ! [ -a $VENDOR ]; then git clone https://github.com/nattolecats/vendor_motorola_cypfr vendor/motorola/cypfr ; fi
+
+VENDOR_PREBUILT=vendor/motorola/cypfr-prebuilt/vendor.img
+if ! [ -a $VENDOR_PREBUILT ]; then 
+    mkdir -p vendor/motorola/cypfr-prebuilt/
+    curl -o $VENDOR_PREBUILT https://media.githubusercontent.com/media/nattolecats/vendor_motorola_cypfr-prebuilt/master/vendor.img
+fi
 
 FELICA=vendor/motorola/cypfr-felica/device.mk
 if ! [ -a $FELICA ]; then git clone https://github.com/nattolecats/vendor_motorola_cypfr-felica vendor/motorola/cypfr-felica ; fi
