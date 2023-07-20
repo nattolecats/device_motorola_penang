c54c0d262b363124ad7cfe4288ecb1eb34806396
lahaina <nattolecats@gmail.com>
2023-07-20 22:59:37 +0900

    cypfr: overlay: All RRO overlays now goes to product instead vendor * We are using prebuilt vendor.

--- resource-overlay/holi/Frameworks/Android.mk
+++ resource-overlay/holi/Frameworks/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_VENDOR_MODULE := true
+LOCAL_PRODUCT_MODULE := true
 LOCAL_CERTIFICATE := platform
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
