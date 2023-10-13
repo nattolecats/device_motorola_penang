89dcb1edbe24168d707fa9f8d8e2b257d4bfc30f
Anand Shekhawat <anandsingh215@yahoo.com>
2023-10-13 14:56:32 +0200

    penang: remove android.hardware.wifi@1.0-service

--- device.mk
+++ device.mk
@@ -516,7 +516,6 @@ PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := strict
 
 # WiFi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
     hostapd \
     libwpa_client \
     libwifi-hal-ctrl \
