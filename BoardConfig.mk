2a642ba22ee2b2845cc2990ed9e6ad5cf2eed834
lahaina <nattolecats@gmail.com>
2023-10-29 00:46:20 +0900

    penang: Fix prebuilt vendor * Not accessible with inherit, use include instead.
    device/motorola/penang/BoardConfig.mk:192: error: cannot assign to readonly variable: PRODUCT_NAME

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -189,7 +189,7 @@ TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 TARGET_COPY_OUT_VENDOR := vendor
 
 # Prebuilt vendor image
-$(call inherit-product, device/motorola/penang-images/vendor-image.mk)
+include device/motorola/penang-images/vendor-image.mk
 
 # DT2W
 TARGET_TAP_TO_WAKE_NODE := "/sys/class/sensors/dt-gesture/enable"
